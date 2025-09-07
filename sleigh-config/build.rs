eec186b43082af9b99d2b1ea0a7016d7ccee2651
mnemonikr <138624285+mnemonikr@users.noreply.github.com>
2025-09-06 17:17:21 -0700

    Add global data variables

--- sleigh-config/build.rs
+++ sleigh-config/build.rs
@@ -10,6 +10,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut config_vars_file = File::create(Path::new(&out_dir).join("config_vars.rs"))?;
 
     let processors_root = "ghidra/Ghidra/Processors";
+    let mut sla_data = Vec::new();
+    let mut pspec_data = Vec::new();
+
     for processor_dir in std::fs::read_dir(Path::new(processors_root))?.filter_map(Result::ok) {
         let processor_name = processor_dir.file_name().to_str().unwrap().to_owned();
         let feature_env_name = processor_name.to_uppercase().replace("-", "_");
@@ -39,25 +42,31 @@ fn main() -> Result<(), Box<dyn Error>> {
                     Some(ext) if ext == "slaspec" => {
                         // File extension is slaspec
                         let output_path = Path::new(&out_dir).join(format!("{file_stem}.sla"));
+                        let var_name = format!("SLA_{var_name}");
 
                         let mut compiler = sleigh_compiler::SleighCompiler::default();
                         compiler.compile(lang_entry.path(), &output_path)?;
                         writeln!(
                             &mut config_vars_file,
-                            r##"pub const SLA_{var_name}: &'static [u8] = include_bytes!(r#"{sla_path}"#);"##,
-                            sla_path = output_path.display()
+                            r##"pub const {var_name}: &'static [u8] = include_bytes!(r#"{path}"#);"##,
+                            path = output_path.display()
                         )?;
+
+                        sla_data.push(format!("{mod_name}::{var_name}"));
                     }
                     Some(ext) if ext == "pspec" => {
                         // File extension is pspec
+                        let var_name = format!("PSPEC_{var_name}");
                         let output_path = Path::new(&out_dir).join(lang_entry.file_name());
                         std::fs::copy(lang_entry.path(), &output_path)?;
 
                         writeln!(
                             &mut config_vars_file,
-                            r##"pub const PSPEC_{var_name}: &'static str = include_str!(r#"{sla_path}"#);"##,
-                            sla_path = output_path.display()
+                            r##"pub const {var_name}: &'static str = include_str!(r#"{path}"#);"##,
+                            path = output_path.display()
                         )?;
+
+                        pspec_data.push(format!("{mod_name}::{var_name}"));
                     }
 
                     // No match, nothing to do
@@ -67,6 +76,26 @@ fn main() -> Result<(), Box<dyn Error>> {
             writeln!(&mut config_vars_file, r#"}}"#)?;
         } // End feature check
     } // End processor directory for loop
+
+    writeln!(
+        &mut config_vars_file,
+        r##"pub const SLA_DATA: [(&'static str, &'static [u8]); {len}] = ["##,
+        len = sla_data.len()
+    )?;
+    for entry in &sla_data {
+        writeln!(&mut config_vars_file, r##"(r#"{entry}"#, &{entry}),"##)?;
+    }
+    writeln!(&mut config_vars_file, r##"];"##)?;
+
+    writeln!(
+        &mut config_vars_file,
+        r##"pub const PSPEC_DATA: [(&'static str, &'static str); {len}] = ["##,
+        len = pspec_data.len()
+    )?;
+    for entry in &pspec_data {
+        writeln!(&mut config_vars_file, r##"(r#"{entry}"#, &{entry}),"##)?;
+    }
+    writeln!(&mut config_vars_file, r##"];"##)?;
 
     Ok(())
 }
